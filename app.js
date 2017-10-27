8b2533bc54b6a04ffbae3c6409125e3268e78286
sarahhealey <sarahdhealey@gmail.com>
2017-10-27 15:28:31 -0600

    modifying knex

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pg = require('pg');
-var knex = require('../db/knex');
+var knex = require('./db/knex');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
