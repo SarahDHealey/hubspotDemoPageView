d341f34ac2aab4941da93bcd657046a7c9fef741
sarahhealey <sarahdhealey@gmail.com>
2017-10-27 15:15:30 -0600

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -2,44 +2,12 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'postgresql',
+  connection: 'postgres://localhost/mod2_todos'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  connection: 'process.env.DATABASE_URL' + '?ssl=true'
   }
-
 };
