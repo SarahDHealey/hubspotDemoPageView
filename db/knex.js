3c54eb9ebef88181c95332cc2263f0422fa2ff3d
sarahhealey <sarahdhealey@gmail.com>
2017-10-27 15:42:03 -0600

    fixed knex

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,3 @@
 const environment = process.NODE_ENV || 'development';
-const config = require(../knexfile)[environment]
+const config = require('../knexfile')[environment]
 module.exports = require('knex')(config);
