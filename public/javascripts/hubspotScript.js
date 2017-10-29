795ca4741a2a0c13b7ae014e43fd66e926b13395
sarahhealey <sarahdhealey@gmail.com>
2017-10-29 11:41:18 -0600

    hubspot code embedded

---
+++ public/javascripts/hubspotScript.js
@@ -0,0 +1,15 @@
+// <!-- Set up the path for the initial page view -->
+
+  var _hsq = window._hsq = window._hsq || [];
+  _hsq.push(['setPath', 'http://localhost:3000']);
+
+
+// <!-- Load the HubSpot tracking code -->
+// <!-- Start of HubSpot Embed Code -->
+// <!-- End of HubSpot Embed Code -->
+// <!-- Tracking subsequent page views -->
+
+  var _hsq = window._hsq = window._hsq || [];
+  _hsq.push(['setPath', '/about-us']);
+  _hsq.push(['trackPageView']);
+  console.log(_hsq)
